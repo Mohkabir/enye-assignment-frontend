95c794391789be40eb84cd2bc2ac5c0a07823b6f
mohammed kabir <kbshowkb@gmail.com>
2021-01-16 21:34:29 -1200

    Updates

---
+++ precache-manifest.2328cdd923356c36957de00f6a93679e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d771f01ba7a2f13c4b20e41f4ecdd7",
+    "url": "/enye-assignment-frontend/index.html"
+  },
+  {
+    "revision": "afc3bc0a7cf5d464c89a",
+    "url": "/enye-assignment-frontend/static/css/main.255bd9b2.chunk.css"
+  },
+  {
+    "revision": "9ca9eb42705c96222cf4",
+    "url": "/enye-assignment-frontend/static/js/2.b0ab3041.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/enye-assignment-frontend/static/js/2.b0ab3041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc3bc0a7cf5d464c89a",
+    "url": "/enye-assignment-frontend/static/js/main.d2a520a5.chunk.js"
+  },
+  {
+    "revision": "1497a196c88dcf79412d",
+    "url": "/enye-assignment-frontend/static/js/runtime-main.29ceed9f.js"
+  }
+]);
